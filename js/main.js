4e3027eaa7a364a261132d44c5a72fccb905d54b
Vladimir <vladimir.alexis@purchase.edu>
2019-02-20 10:06:28 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*$(document).ready(function(){
+  $("#hide").click(function(){
+    $("p").hide();
+  });
+  $("#show").click(function(){
+    $("p").show();
+  });
+});
+*/
+/*  $(document).ready(function () {
+          $('#clown').click(function () {
+              $(this)
+                  .animate({ 'left': '300' })
+                  .animate({ 'top': '200' })
+                  .animate({ 'left': '10' })
+                  .animate({ 'top': '10' });
+          });
+      });
+      */
